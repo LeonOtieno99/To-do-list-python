2a2442a931ab6b47b1bab431229b1f8a3e90ba13
leon <leoncifer99@gmail.com>
2023-09-20 18:25:05 +0300

    todolist-python

--- todolist.py
+++ todolist.py
@@ -1,50 +1,71 @@
 import os
 import json
 
-print('\t\t\t***********************************************')
-print('\t\t\t*      To do list Command line Application    *')
-print('\t\t\t***********************************************')
-
+#main function
 def tasks():
-    print('\t\t\t\tAvailable choices:')
-    print('\t\t\t\t\t1.View Tasks')
-    print('\t\t\t\t\t2.Add Tasks')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\t\t***********************************************')
+    print('\t\t*      To do list Command line Application    *')
+    print('\t\t***********************************************')
+
+    print('\t\t\tAvailable choices:')
+    print('\t\t\t\t1.Add Tasks')
+    print('\t\t\t\t2.View Tasks')
+   
     selected = int(input('Enter value the value of your choice:'))
 
     if selected == 1:
         os.system('cls' if os.name == 'nt' else 'clear')
-        viewTasks()
+        addTasks()
     elif selected == 2:
         os.system('cls' if os.name == 'nt' else 'clear')
-        addTasks()
+        viewTasks()
     else:
         print('Invalid choice')
         print('Please enter 1 for the first option or 2 for the second option')
         tasks()
-
-
-def addTasks():
-    print('\t\t\t***********************************************')
-    print('\t\t\t*                  Add Task                   *')
-    print('\t\t\t***********************************************')
+#load from json file
+def loadfromjson(data):
     task_name = input('Please enter the task?\n')
     status = "not completed"
     new_task = {"task": task_name, "status": status}
-    with open("tasks.json", "a+") as json_file:
-        json.dump(new_task, json_file, indent=4)
-        json_file.write("\n")
+    data.append(new_task)
+    return data
 
+
+#add tasks function
+def addTasks():
+    print('\t\t***********************************************')
+    print('\t\t*                  Add Task                   *')
+    print('\t\t***********************************************')
+    
+    file_path = "tasks.json"
+    if os.path.exists(file_path):
+        file_size = os.path.getsize(file_path)
+        if file_size > 0:
+                with open("tasks.json","r") as json_file:
+                    data = json.load(json_file)
+                    loadfromjson(data)
+                with open("tasks.json", "w") as json_file:
+                    json.dump(data, json_file, indent=4)
+        else:
+            data = []
+            loadfromjson(data)
+            with open("tasks.json", "w") as json_file:
+                json.dump(data, json_file, indent=4)
+    tasks()
+#view task function
 def viewTasks():
-    print('\t\t\t***********************************************')
-    print('\t\t\t*              All your Tasks                 *')
-    print('\t\t\t***********************************************')
+    print('\t\t***********************************************')
+    print('\t\t*              All your Tasks                 *')
+    print('\t\t***********************************************')
 
     #Load existing files from the JSON file or initilizing
     try:
         with open("tasks.json", "r") as json_file:
             tasks = json.load(json_file)
     except FileNotFoundError:
-        tasks = []
+        print("File is empty")
 
     #show tasks
     print("Tasks:")
@@ -56,23 +77,19 @@ def viewTasks():
     task_name = input("Enter the name of the task you want to update: ")
 
     for task in tasks:
-        if task["task"] == task_name:
+        if task['task'] == task_name:
             new_status = input("Mark as completed? (yes or no): ")
             if new_status.lower() == "yes":
                 task["status"] = "completed"
+                print("Task marked as completed")
             elif new_status.lower() == "no":
                 task["status"] = "not completed"
+                print("Task marked as not completed")
             else:
-                print("invalid choice")
-                break
-        else:
-            print(f"Task '{task_name}' not found.")
+               print(f"Task '{task_name}' not found.")
 
     #save the updated list
     with open("tasks.json", "w") as json_file:
         json.dump(tasks, json_file, indent=4)
-
-    print("Goodbye!")
-
 
 tasks()
